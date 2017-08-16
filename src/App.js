6793c95f4fdf63194ccc623be25a4d6cf4b558a7
Svitlana Dzyuban <svitlana.dzyuban@gmail.com>
2017-08-16 18:43:21 +0800

    first and last commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '/** Add your JSX here **/',
+      output: '',
+      error: ''
+    }
+  }
+
+  update(e) {
+    let code = e.target.value;
+    try {
+      this.setState({
+        output: window.Babel
+          .transform(code, {presets: ['es2015', 'react']})
+          .code,
+        error: ''
+      })
+    } catch (error) {
+      this.setState({error: error.message})
+    }
+
+  }
+
+  render() {
+    return (
+      <div>
+        <header>{this.state.error}</header>
+        <div className="container">
+          <textarea
+            onChange={this.update.bind(this)}
+            defaultValue={this.state.input}
+          />
+            <pre>{this.state.output}</pre>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
