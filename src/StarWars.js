cf48b6af0f16d85e09a3ccb1c54db4913b76ce9f
Svitlana Dzyuban <svitlana.dzyuban@gmail.com>
2017-08-17 11:09:53 +0800

    StarWars names fetch and filter example

---
+++ src/StarWars.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+class StarWarsNames extends Component {
+  constructor() {
+    super();
+
+    this.state = {items: []};
+  }
+
+  componentWillMount(){
+    fetch('http://swapi.co/api/people/?format=json')
+      .then( response => response.json() )
+      .then( ({results: items}) => this.setState({items}) )
+  }
+
+  filter(e) {
+    this.setState({filter: e.target.value})
+  }
+
+  render() {
+    let items = this.state.items;
+    if (this.state.filter) {
+      items = items.filter(item =>
+        item.name.toLowerCase()
+          .includes(this.state.filter.toLowerCase())
+      )
+    }
+    return(
+      <div>
+        <input type="text" onChange={this.filter.bind(this)}/>
+        {items.map(item => <Person key={item.name} name={item.name} />)}
+      </div>)
+  }
+}
+
+const Person = (props) => <h4>{props.name}</h4>;
+
+export default StarWarsNames;
